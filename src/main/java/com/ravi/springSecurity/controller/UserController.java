1763116b6891126a4998157a318b41a9f1b4ce11
Ravi <ravipandit2001.11.02@gmail.com>
2025-07-03 07:21:23 +0545

    authentication

--- src/main/java/com/ravi/springSecurity/controller/UserController.java
+++ src/main/java/com/ravi/springSecurity/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
